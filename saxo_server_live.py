d914bd41ae82436583672d6ebbb10154f4643edb
CHimerioN <chimerionk@gmail.com>
2021-08-20 14:17:57 +0200

    Added paramiko to send file

--- saxo_server_live.py
+++ saxo_server_live.py
@@ -1,13 +1,13 @@
 import socket
 import sys
 import requests
-from typing import final
-from email import message_from_string
+from sys import argv
 import re
 import json
 import pandas as pd
 import datetime
 import webbrowser
+import paramiko
 # SPECIFYING HOST AND PORT
 HOST = ''
 PORT = 5000
@@ -24,8 +24,7 @@ except socket.error as message:
     sys.exit()
 
 soc.listen(1)
-print("Opening site...")
-webbrowser.open('LIVE LOGONVALIDATION SITE')
+webbrowser.open('https://live.logonvalidation.net:443/login/?requestId=NDQ0Y2EwMzIwNjk3NDJiNTk2ZDUyMmI3NmY4OGE1ZmUyOTI2MTg1&mode=oauth')
 # LISTENING TO LOG IN PROCESS AND REQUESTING AUTHORIZATION CODE
 
 while True:
@@ -37,7 +36,7 @@ while True:
 
         while True:
             data = conn.recv(4096)
-            #print('received "%s"' % data)
+            #print('received "%s"' % data)  # uncomment if you want to see what is received
             response = data.decode("utf-8")
             m = re.search('code=(.+?)&state',response)
             if m:
@@ -50,6 +49,8 @@ while True:
         conn.close()
         break
 
+#print("#2 CODE:_",found)
+
 # REQUESTING TOKEN
 headers = {
     'Content-type': 'application/x-www-form-urlencoded',
@@ -58,13 +59,13 @@ headers = {
 data = {
   'grant_type': 'authorization_code',
   'code':found,
-  'redirect_uri': 'URL FOR REDIRECT',
-  'client_id': 'APP_KEY',
-  'client_secret': 'APP_SECRET'
+  'redirect_uri': 'http://localhost:5000/liveapp',
+  'client_id': 'APP KEY',
+  'client_secret': 'APP SECRET'
 }
 
 response = requests.post('https://live.logonvalidation.net/token', headers=headers, data=data, verify=False)
-print(response.content)
+#print(response.content)
 
 x = response.content.decode("utf-8")
 y = re.search('access_token":"(.+?)","',x)
@@ -77,11 +78,33 @@ headers = {
     'Content-Type': 'application/x-www-form-urlencoded',
 }
 
-response = requests.get('https://gateway.saxobank.com/openapi/port/v1/positions/', headers=headers, verify=False)
+response = requests.get('https://gateway.saxobank.com/openapi/port/v1/positions/?FieldGroups=greeks,exchangeinfo,positionbase,positionview&ClientKey=x9RQ1PMdoLmK|X8oij-MGw==', headers=headers, verify=False)
 json_data = json.loads(response.text)
 print(json.dumps(json_data,indent=4))
 
 # SAVING DATA TO CSV FILE
 df = pd.json_normalize(json_data['Data'])
-filename = "SAXO_POS_"+datetime.datetime.today().strftime(':%Y_%m_%d') + ".csv"
+filename = "SAXO_POS_"+datetime.datetime.today().strftime('%Y_%m_%d') + ".csv"
 df.to_csv(filename,header=True,index=None)
+print("New file"+filename+" has been created.")
+
+# CREATING SSH TUNNEL TO SEND FILE
+ssh_client = paramiko.SSHClient()
+ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh_client.connect(hostname="dev.mentordata.com",username=argv[1],password=argv[2])
+
+target = "/home/lfa/saxo/dane/"+filename
+ftp_client = ssh_client.open_sftp()
+ftp_client.put(filename,target)
+print("Ssh connection created. File ["+filename+"] has been uploaded.")
+ftp_client.close()
+
+
+
+stdin,stdout,stderr=ssh_client.exec_command("ls /home/lfa/saxo/dane")
+for i in stdout:
+    print(i)
+
+
+
+ssh_client.close()
