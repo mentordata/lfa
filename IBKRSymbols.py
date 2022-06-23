eeba090f52312be628fb35b144956119ff692e40
CHimerioN <chimerionk@gmail.com>
2022-06-23 09:29:47 +0200

    Update DAX contract - end 2022

--- IBKRSymbols.py
+++ IBKRSymbols.py
@@ -40,48 +40,53 @@ def getSymbols(asset, expiration, start_strike, number, option_type):
                 _dex_desc = "ODX5"
                 print(_dex_desc)
         _temp_num = 0
-        _curr_strike = start_strike+_temp_num
         for i in range(number):
             if option_type == "CALL" or option_type == "call":
-                _name = "C "+str(_dex_desc).upper()+" "+str(_month).upper()+" "+str(_year)+" "+str((_curr_strike))
+                _name = "C "+str(_dex_desc).upper()+" "+str(_month).upper()+" "+str(_year)+" "+str((start_strike+_temp_num))
             if option_type == "PUT" or option_type == "put":
-                _name = "P "+str(_dex_desc).upper()+" "+str(_month).upper()+" "+str(_year)+" "+str((_curr_strike))
+                _name = "P "+str(_dex_desc).upper()+" "+str(_month).upper()+" "+str(_year)+" "+str((start_strike+_temp_num))
             
-            df.loc[i] = [_name,(_curr_strike),"DTB",str("OPT").upper()] 
+            df.loc[i] = [_name,(start_strike+_temp_num),"DTB",str("OPT").upper()] 
             
             if date_time_obj.strftime('%Y-%m-%d') == "2022-06-24" or date_time_obj.strftime('%Y-%m-%d') == "2022-07-01" or date_time_obj.strftime('%Y-%m-%d') == "2022-07-08" or date_time_obj.strftime('%Y-%m-%d') == "2022-07-29":
-                if (_curr_strike) >= 11800:
+                if (start_strike+_temp_num) >= 11800:
                     _temp_num+=50
                     continue
             if date_time_obj.strftime('%Y-%m-%d') == "2022-07-15":
-                if (_curr_strike) == 8000:
+                if (start_strike+_temp_num) == 8000:
                     _temp_num+=1000
                     continue
-                if (_curr_strike) >= 9000 and (_curr_strike) < 11800:
+                if (start_strike+_temp_num) >= 9000 and (start_strike+_temp_num) < 11800:
                     _temp_num+=200
                     continue
-                if (_curr_strike) >= 11800:
+                if (start_strike+_temp_num) >= 11800:
                     _temp_num+=50
                     continue
             if date_time_obj.strftime('%Y-%m-%d') == "2022-08-19":
-                if (_curr_strike) == 15900:
+                if (start_strike+_temp_num) == 15900:
                     _temp_num+=100
                     continue
-                if ((_curr_strike) >= 8000 and (_curr_strike) < 11800) or ((_curr_strike) >= 16000 and (_curr_strike) < 18000):
+                if ((start_strike+_temp_num) >= 8000 and (start_strike+_temp_num) < 11800) or ((start_strike+_temp_num) >= 16000 and (start_strike+_temp_num) < 18000):
                     _temp_num+=200
                     continue
-                if (_curr_strike) >= 11800 and (_curr_strike) < 15900:
+                if (start_strike+_temp_num) >= 11800 and (start_strike+_temp_num) < 15900:
                     _temp_num+=50
                     continue
             if date_time_obj.strftime('%Y-%m-%d') == "2022-09-16": # to fix
-                if (_curr_strike) == 2000:
+                if (start_strike+_temp_num) == 2000:
                     _temp_num+=2500
                     continue
-                if (_curr_strike) >= 4500 and (_curr_strike) < 10000:
+                if (start_strike+_temp_num) >= 4500 and (start_strike+_temp_num) < 10000:
                     _temp_num+=500
                     continue
-                if ((_curr_strike) >= 10000 and (_curr_strike) < 10800) or ((_curr_strike) >= 16000 and (_curr_strike) < 18000):
+                if ((start_strike+_temp_num) >= 10000 and (start_strike+_temp_num) < 10800) or ((start_strike+_temp_num) >= 17200 and (start_strike+_temp_num) < 20000):
                     _temp_num+=200
+                    continue
+                if ((start_strike+_temp_num) >= 10800 and (start_strike+_temp_num) < 11900) or ((start_strike+_temp_num) >= 14500 and (start_strike+_temp_num) < 17200):
+                    _temp_num+=100
+                    continue
+                if ((start_strike+_temp_num) >= 11900 and (start_strike+_temp_num) < 14500):
+                    _temp_num+=50
                     continue
                 
             
@@ -89,33 +94,32 @@ def getSymbols(asset, expiration, start_strike, number, option_type):
         print(df)
     if asset == "SPX":
         _temp_num = 0
-        _curr_strike = start_strike+_temp_num
         for i in range(number):
             if option_type == "CALL" or option_type == "call":
-                _name = "SPXW "+str(_year)+str(_month_num).upper()+str(_day).upper()+"C0"+str((_curr_strike))+"000"
+                _name = "SPXW "+str(_year)+str(_month_num).upper()+str(_day).upper()+"C0"+str((start_strike+_temp_num))+"000"
             if option_type == "PUT" or option_type == "put":
-                _name = "SPXW "+str(_year)+str(_month_num).upper()+str(_day).upper()+"P0"+str((_curr_strike))+"000"
+                _name = "SPXW "+str(_year)+str(_month_num).upper()+str(_day).upper()+"P0"+str((start_strike+_temp_num))+"000"
             
-            df.loc[i] = [_name,(_curr_strike),"CBOE",str("OPT").upper()] 
-            if (_curr_strike) < 2800 or ((_curr_strike) >= 4600 and (_curr_strike) <= 5000):
+            df.loc[i] = [_name,(start_strike+_temp_num),"CBOE",str("OPT").upper()] 
+            if (start_strike+_temp_num) < 2800 or ((start_strike+_temp_num) >= 4600 and (start_strike+_temp_num) <= 5000):
                 _temp_num+=200
                 continue
-            if (_curr_strike) == 2800 or ((_curr_strike) >= 4300 and (_curr_strike) < 4600):
+            if (start_strike+_temp_num) == 2800 or ((start_strike+_temp_num) >= 4300 and (start_strike+_temp_num) < 4600):
                 _temp_num+=100
                 continue
-            if (_curr_strike) >= 2900 and (_curr_strike) < 3350:
+            if (start_strike+_temp_num) >= 2900 and (start_strike+_temp_num) < 3350:
                 _temp_num+=50
                 continue
-            if ((_curr_strike) >= 3350 and (_curr_strike) < 3425) or ((_curr_strike) >= 4250 and (_curr_strike) < 4300):
+            if ((start_strike+_temp_num) >= 3350 and (start_strike+_temp_num) < 3425) or ((start_strike+_temp_num) >= 4250 and (start_strike+_temp_num) < 4300):
                 _temp_num+=25
                 continue
-            if (_curr_strike) == 3425:
+            if (start_strike+_temp_num) == 3425:
                 _temp_num+=15
                 continue
-            if ((_curr_strike) >= 3440 and (_curr_strike) < 3470) or ((_curr_strike) >= 3480 and (_curr_strike) < 3510) or ((_curr_strike) >= 4180 and (_curr_strike) < 4220) or ((_curr_strike) >= 4230 and (_curr_strike) < 4250):
+            if ((start_strike+_temp_num) >= 3440 and (start_strike+_temp_num) < 3470) or ((start_strike+_temp_num) >= 3480 and (start_strike+_temp_num) < 3510) or ((start_strike+_temp_num) >= 4180 and (start_strike+_temp_num) < 4220) or ((start_strike+_temp_num) >= 4230 and (start_strike+_temp_num) < 4250):
                 _temp_num+=10
                 continue
-            if ((_curr_strike) >= 3470 and (_curr_strike) < 3480) or ((_curr_strike) >= 3510 and (_curr_strike) < 4180) or ((_curr_strike) >= 4220 and (_curr_strike) < 4230):
+            if ((start_strike+_temp_num) >= 3470 and (start_strike+_temp_num) < 3480) or ((start_strike+_temp_num) >= 3510 and (start_strike+_temp_num) < 4180) or ((start_strike+_temp_num) >= 4220 and (start_strike+_temp_num) < 4230):
                 _temp_num+=5
                 continue
 
@@ -130,62 +134,60 @@ def getSymbols(asset, expiration, start_strike, number, option_type):
         pass
     if asset == "V2EU":
         _temp_num = 0
-        _curr_strike = start_strike+_temp_num
         for i in range(number):
             if option_type == "CALL" or option_type == "call":
-                _name = "C OVS2"+str(_month).upper()+str(_year)+str((_curr_strike))
+                _name = "C OVS2"+str(_month).upper()+str(_year)+str((start_strike+_temp_num))
             if option_type == "PUT" or option_type == "put":
-                _name = "P OVS2 "+str(_month).upper()+" "+str(_year)+" "+str((_curr_strike))
+                _name = "P OVS2 "+str(_month).upper()+" "+str(_year)+" "+str((start_strike+_temp_num))
             
-            df.loc[i] = [_name,(_curr_strike),"DTB",str("OPT").upper()] 
-            if (_curr_strike) < 15:
+            df.loc[i] = [_name,(start_strike+_temp_num),"DTB",str("OPT").upper()] 
+            if (start_strike+_temp_num) < 15:
                 _temp_num+=0.5
                 continue
-            if (_curr_strike) >= 15 and (_curr_strike) < 30:
+            if (start_strike+_temp_num) >= 15 and (start_strike+_temp_num) < 30:
                 _temp_num+=1
                 continue
-            if (_curr_strike) >= 30 and (_curr_strike) < 50:
+            if (start_strike+_temp_num) >= 30 and (start_strike+_temp_num) < 50:
                 _temp_num+=2
                 continue
-            if (_curr_strike) >= 50 and (_curr_strike) < 100:
+            if (start_strike+_temp_num) >= 50 and (start_strike+_temp_num) < 100:
                 _temp_num+=5
                 continue
-            if (_curr_strike) >= 100:
+            if (start_strike+_temp_num) >= 100:
                 _temp_num+=10
         print(df)
     if asset == "STOXX":
         _temp_num = 0
-        _curr_strike = start_strike+_temp_num
         for i in range(number):
             if option_type == "CALL" or option_type == "call":
-                _name = "C OEXD "+str(_month).upper()+" "+str(_year)+" "+str((_curr_strike))
+                _name = "C OEXD "+str(_month).upper()+" "+str(_year)+" "+str((start_strike+_temp_num))
             if option_type == "PUT" or option_type == "put":
-                _name = "P OEXD "+str(_month).upper()+" "+str(_year)+" "+str((_curr_strike))
+                _name = "P OEXD "+str(_month).upper()+" "+str(_year)+" "+str((start_strike+_temp_num))
             
-            df.loc[i] = [_name,(_curr_strike),"DTB",str(option_type).upper()] 
-            if (_curr_strike) < 80 or ((_curr_strike) >= 85 and (_curr_strike) < 100):
+            df.loc[i] = [_name,(start_strike+_temp_num),"DTB",str(option_type).upper()] 
+            if (start_strike+_temp_num) < 80 or ((start_strike+_temp_num) >= 85 and (start_strike+_temp_num) < 100):
                 _temp_num+=5
                 continue
-            if (_curr_strike) == 80 or (_curr_strike) == 132:
+            if (start_strike+_temp_num) == 80 or (start_strike+_temp_num) == 132:
                 _temp_num+=3
                 continue
-            if (_curr_strike) == 83 or (_curr_strike) == 100 or (_curr_strike) == 110 or (_curr_strike) == 130:
+            if (start_strike+_temp_num) == 83 or (start_strike+_temp_num) == 100 or (start_strike+_temp_num) == 110 or (start_strike+_temp_num) == 130:
                 _temp_num+=2
                 continue
-            if (_curr_strike) >= 102 and (_curr_strike) < 110:
+            if (start_strike+_temp_num) >= 102 and (start_strike+_temp_num) < 110:
                 _temp_num+=1
                 continue
-            if (_curr_strike) >= 112 and (_curr_strike) < 125:
+            if (start_strike+_temp_num) >= 112 and (start_strike+_temp_num) < 125:
                 _temp_num+=1
                 continue
-            if (_curr_strike) == 125 or (_curr_strike) == 135:
+            if (start_strike+_temp_num) == 125 or (start_strike+_temp_num) == 135:
                 _temp_num+=5
                 continue
-            if (_curr_strike) == 140:
+            if (start_strike+_temp_num) == 140:
                 _temp_num+=10
                 continue
         print(df)
 
     
-getSymbols("DAX","2022.06.24",20,10,"call")
+getSymbols("DAX","2022.09.16",11800,20,"call")
     
