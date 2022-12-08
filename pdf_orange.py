9b10e3c933634ab6f374eb104d5378a7318884a8
CHimerioN <chimerionk@gmail.com>
2022-12-08 14:12:53 +0100

    Added exception printout

--- pdf_orange.py
+++ pdf_orange.py
@@ -66,121 +66,124 @@ for filename in os.listdir(sys.argv[1]):
                     file_version = 2
 
     # main part of program
+    try:
+        temp_list = []
+        if(file_version == 1):
+            for line in lines:
+                line.strip()
+                line_parts = line.split()
 
-    temp_list = []
-    if(file_version == 1):
-        for line in lines:
-            line.strip()
-            line_parts = line.split()
-
-            if (len(line_parts)>=9 and re.search('brutto za telefon',line.strip())):
-                number = re.search(r'za telefon nr (.*)',line.strip()).group(1)
-                current_client_number = int(number[0:9])
-            if(len(line_parts) == 2 and '.' in line_parts[0] and line_parts[0][0].isdigit() and len(line_parts[0])== 11):
-                _date = datetime.datetime.strptime(str(line_parts[0][:-1]), '%d.%m.%Y').date()
-            if(len(line_parts) == 6):
-                if(":" in line_parts[0]):
-                    _hour = datetime.datetime.strptime(str(line_parts[0][0:5]), '%H:%M').time()
-                else:
-                    continue
-                _network = line_parts[0][6:]
-                if(line_parts[-4] != '1'):
-                    resultFile.write(_date.isoformat()+","+_hour.isoformat()+","+str(current_client_number)+","+_network+",call,"+line_parts[-5]+","+line_parts[-4]+","+line_parts[-1].replace(",",".")+"\n")
-                    row_counter+=1
-                else:
-                    resultFile.write(_date.isoformat()+","+_hour.isoformat()+","+str(current_client_number)+","+_network+",sms,"+line_parts[-5]+","+line_parts[-4]+","+line_parts[-1].replace(",",".")+"\n")
-                    row_counter+=1
-            if(len(line_parts) == 7):
-                if(":" in line_parts[0]):
-                    _hour = datetime.datetime.strptime(str(line_parts[0][0:5]), '%H:%M').time()
-                else:
-                    continue
-                if(len(line_parts[0]) == 7):    #Polska
-                    _network = line_parts[1]
-                else:
-                    if(line_parts[1] == "mobile"):
-                        _network = "nju mobile"
-                    if(line_parts[1] == "P4"):
-                        _network = "sieć P4"
-                    if(line_parts[1] == "Polkomtel"):
-                        _network = "sieć Polkomtel"
-                    if(line_parts[1] == "T-mobile"):
-                        _network = "sieć T-mobile"
-                
-                if(line_parts[-4] != '1'):
-                    resultFile.write(_date.isoformat()+","+_hour.isoformat()+","+str(current_client_number)+","+_network+",call,"+line_parts[-5]+","+line_parts[-4]+","+line_parts[-1].replace(",",".")+"\n")
-                    row_counter+=1
-                else:
-                    resultFile.write(_date.isoformat()+","+_hour.isoformat()+","+str(current_client_number)+","+_network+",sms,"+line_parts[-5]+","+line_parts[-4]+","+line_parts[-1].replace(",",".")+"\n")
-                    row_counter+=1
-            if(len(line_parts) == 8 or len(line_parts) == 9):
-                if("," not in line_parts[-1] or len(line_parts[-5]) != 9):
-                    continue
-                _hour = datetime.datetime.strptime(str(line_parts[0][0:5]), '%H:%M').time()
-
-                if(len(line_parts[3])==9):
-                    if(line_parts[2] == "mobile"):
-                        _network = "nju mobile"
-                    if(line_parts[2] == "P4"):
-                        _network = "sieć P4"
-                    if(line_parts[2] == "Polkomtel"):
-                        _network = "sieć Polkomtel"
-                    if(line_parts[2] == "T-mobile"):
-                        _network = "sieć T-mobile"
-                else:
-                    _network = line_parts[2]+" "+line_parts[3]
-                
-                if(line_parts[-4] != '1'):
-                    resultFile.write(_date.isoformat()+","+_hour.isoformat()+","+str(current_client_number)+","+_network+",call,"+line_parts[-5]+","+line_parts[-4]+","+line_parts[-1].replace(",",".")+"\n")
-                    row_counter+=1
-                else:
-                    resultFile.write(_date.isoformat()+","+_hour.isoformat()+","+str(current_client_number)+","+_network+",sms,"+line_parts[-5]+","+line_parts[-4]+","+line_parts[-1].replace(",",".")+"\n")
-                    row_counter+=1
-    if(file_version == 2):
-        for line in lines:
-            line.strip()
-            line_parts = line.split()
-
-            if (len(line_parts)>=9 and re.search('brutto za telefon',line.strip())):
-                number = re.search(r'za telefon nr (.*)',line.strip()).group(1)
-                current_client_number = int(number[0:9])
-            if(len(line_parts) == 1):
-                if(':' in line_parts[0] and hasNumbers(line_parts[0])):
-                    if len(temp_list) == 1:
-                        temp_list.append(line_parts)
-                    if len(temp_list) >= 2:
-                        temp_list[1] = line_parts
-            if(len(line_parts) == 2):
-                if(len(line_parts) == 2 and '.' in line_parts[0] and line_parts[0][0].isdigit()):
-                    if temp_list:
-                            temp_list[0] = line_parts
+                if (len(line_parts)>=9 and re.search('brutto za telefon',line.strip())):
+                    number = re.search(r'za telefon nr (.*)',line.strip()).group(1)
+                    current_client_number = int(number[0:9])
+                if(len(line_parts) == 2 and '.' in line_parts[0] and line_parts[0][0].isdigit() and len(line_parts[0])== 11):
+                    _date = datetime.datetime.strptime(str(line_parts[0][:-1]), '%d.%m.%Y').date()
+                if(len(line_parts) == 6):
+                    if(":" in line_parts[0]):
+                        _hour = datetime.datetime.strptime(str(line_parts[0][0:5]), '%H:%M').time()
                     else:
-                        temp_list.append(line_parts)
-            if(len(line_parts) > 2):
-                if(len(line_parts) == 6 or len(line_parts) == 7 or len(line_parts) == 8):
-                    if((len(line_parts[1]) == 9 or len(line_parts[2]) == 9 ) and (line_parts[1].isdigit() or line_parts[2].isdigit())):
-                        merge_net(line_parts)
-                        if len(temp_list) == 2:
-                            temp_list.append(line_parts)
-                        if len(temp_list) > 2:
-                            temp_list[2] = line_parts
-                        x = [item for sublist in temp_list for item in sublist]
-                        x[0] = x[0].rstrip(',')
-                        #print(x)
-                        if(x[0][2]=='.'):
-                            _date = datetime.datetime.strptime(str(x[0]), '%d.%m.%Y').date()
-                        else:
-                            _date = datetime.datetime.strptime(str(x[0]), '%Y.%m.%d').date()
-                        _hour = datetime.datetime.strptime(str(x[2]), '%H:%M').time()
-                        _network = x[3]
-                        if(x[5] != '1'):
-                            resultFile.write(_date.isoformat()+","+_hour.isoformat()+","+str(current_client_number)+","+_network+",call,"+x[4]+","+x[5]+","+x[-1].replace(",",".")+"\n")
-                            row_counter+=1
-                        else:
-                            resultFile.write(_date.isoformat()+","+_hour.isoformat()+","+str(current_client_number)+","+_network+",sms,"+x[4]+","+x[5]+","+x[-1].replace(",",".")+"\n")
-                            row_counter+=1
+                        continue
+                    _network = line_parts[0][6:]
+                    if(line_parts[-4] != '1'):
+                        resultFile.write(_date.isoformat()+","+_hour.isoformat()+","+str(current_client_number)+","+_network+",call,"+line_parts[-5]+","+line_parts[-4]+","+line_parts[-1].replace(",",".")+"\n")
+                        row_counter+=1
+                    else:
+                        resultFile.write(_date.isoformat()+","+_hour.isoformat()+","+str(current_client_number)+","+_network+",sms,"+line_parts[-5]+","+line_parts[-4]+","+line_parts[-1].replace(",",".")+"\n")
+                        row_counter+=1
+                if(len(line_parts) == 7):
+                    if(":" in line_parts[0]):
+                        _hour = datetime.datetime.strptime(str(line_parts[0][0:5]), '%H:%M').time()
+                    else:
+                        continue
+                    if(len(line_parts[0]) == 7):    #Polska
+                        _network = line_parts[1]
+                    else:
+                        if(line_parts[1] == "mobile"):
+                            _network = "nju mobile"
+                        if(line_parts[1] == "P4"):
+                            _network = "sieć P4"
+                        if(line_parts[1] == "Polkomtel"):
+                            _network = "sieć Polkomtel"
+                        if(line_parts[1] == "T-mobile"):
+                            _network = "sieć T-mobile"
+                    
+                    if(line_parts[-4] != '1'):
+                        resultFile.write(_date.isoformat()+","+_hour.isoformat()+","+str(current_client_number)+","+_network+",call,"+line_parts[-5]+","+line_parts[-4]+","+line_parts[-1].replace(",",".")+"\n")
+                        row_counter+=1
+                    else:
+                        resultFile.write(_date.isoformat()+","+_hour.isoformat()+","+str(current_client_number)+","+_network+",sms,"+line_parts[-5]+","+line_parts[-4]+","+line_parts[-1].replace(",",".")+"\n")
+                        row_counter+=1
+                if(len(line_parts) == 8 or len(line_parts) == 9):
+                    if("," not in line_parts[-1] or len(line_parts[-5]) != 9):
+                        continue
+                    _hour = datetime.datetime.strptime(str(line_parts[0][0:5]), '%H:%M').time()
 
-    print(">>> Processed "+str(row_counter)+" rows.")
+                    if(len(line_parts[3])==9):
+                        if(line_parts[2] == "mobile"):
+                            _network = "nju mobile"
+                        if(line_parts[2] == "P4"):
+                            _network = "sieć P4"
+                        if(line_parts[2] == "Polkomtel"):
+                            _network = "sieć Polkomtel"
+                        if(line_parts[2] == "T-mobile"):
+                            _network = "sieć T-mobile"
+                    else:
+                        _network = line_parts[2]+" "+line_parts[3]
+                    
+                    if(line_parts[-4] != '1'):
+                        resultFile.write(_date.isoformat()+","+_hour.isoformat()+","+str(current_client_number)+","+_network+",call,"+line_parts[-5]+","+line_parts[-4]+","+line_parts[-1].replace(",",".")+"\n")
+                        row_counter+=1
+                    else:
+                        resultFile.write(_date.isoformat()+","+_hour.isoformat()+","+str(current_client_number)+","+_network+",sms,"+line_parts[-5]+","+line_parts[-4]+","+line_parts[-1].replace(",",".")+"\n")
+                        row_counter+=1
+        if(file_version == 2):
+            for line in lines:
+                line.strip()
+                line_parts = line.split()
+
+                if (len(line_parts)>=9 and re.search('brutto za telefon',line.strip())):
+                    number = re.search(r'za telefon nr (.*)',line.strip()).group(1)
+                    current_client_number = int(number[0:9])
+                if(len(line_parts) == 1):
+                    if(':' in line_parts[0] and hasNumbers(line_parts[0])):
+                        if len(temp_list) == 1:
+                            temp_list.append(line_parts)
+                        if len(temp_list) >= 2:
+                            temp_list[1] = line_parts
+                if(len(line_parts) == 2):
+                    if(len(line_parts) == 2 and '.' in line_parts[0] and line_parts[0][0].isdigit()):
+                        if temp_list:
+                                temp_list[0] = line_parts
+                        else:
+                            temp_list.append(line_parts)
+                if(len(line_parts) > 2):
+                    if(len(line_parts) == 6 or len(line_parts) == 7 or len(line_parts) == 8):
+                        if((len(line_parts[1]) == 9 or len(line_parts[2]) == 9 ) and (line_parts[1].isdigit() or line_parts[2].isdigit())):
+                            merge_net(line_parts)
+                            if len(temp_list) == 2:
+                                temp_list.append(line_parts)
+                            if len(temp_list) > 2:
+                                temp_list[2] = line_parts
+                            x = [item for sublist in temp_list for item in sublist]
+                            x[0] = x[0].rstrip(',')
+                            #print(x)
+                            if(x[0][2]=='.'):
+                                _date = datetime.datetime.strptime(str(x[0]), '%d.%m.%Y').date()
+                            else:
+                                _date = datetime.datetime.strptime(str(x[0]), '%Y.%m.%d').date()
+                            _hour = datetime.datetime.strptime(str(x[2]), '%H:%M').time()
+                            _network = x[3]
+                            if(x[5] != '1'):
+                                resultFile.write(_date.isoformat()+","+_hour.isoformat()+","+str(current_client_number)+","+_network+",call,"+x[4]+","+x[5]+","+x[-1].replace(",",".")+"\n")
+                                row_counter+=1
+                            else:
+                                resultFile.write(_date.isoformat()+","+_hour.isoformat()+","+str(current_client_number)+","+_network+",sms,"+x[4]+","+x[5]+","+x[-1].replace(",",".")+"\n")
+                                row_counter+=1
+    except Exception as e:
+        print("[ERROR] ",sys.exc_info()[0]," occured in file: ",f)
+        
+        print("Proceeding to next file...\n")
+    print(">>> Processed "+str(row_counter)+" rows.\n")
     
     file.close()
 print("Program finished.")
